2869591710b25fd263541db8116359e793dbc5a2
Björn Hallberg <bjorn.hallberg@hm.com>
2020-11-05 15:52:18 +0100

    Must have features

--- src/main/java/com/example/Fashionista/FashionistaController.java
+++ src/main/java/com/example/Fashionista/FashionistaController.java
@@ -6,6 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class FashionistaController {
 
@@ -25,17 +29,64 @@ public class FashionistaController {
     }
 
     @GetMapping("/product")
-    public String product(Model model, @RequestParam(required = false, defaultValue = "0") Long id) {
+    public String product(Model model, HttpSession session,
+                          @RequestParam(required = false, defaultValue = "0") Long id,
+                          @RequestParam(required = false, defaultValue = "0") int add) {
+
+        boolean addedToCart = false;
+        if (add > 0) {
+            List<CartItem> cart = (List<CartItem>) session.getAttribute("cart");
+            if (cart == null)
+                cart = new ArrayList<>();
+            cart.add(new CartItem(repository.getProduct(id), add));
+            session.setAttribute("cart", cart);
+            addedToCart = true;
+        }
+
         model.addAttribute("product", repository.getProduct(id));
+        model.addAttribute("addedToCart", addedToCart);
 
         return "product";
     }
 
     @GetMapping("/cart")
-    public String cart(Model model) {
-        //model.addAttribute("cartitems", cartitems);
-        //model.addAttribute("totalamount", totalamount);
+    public String cart(Model model, HttpSession session,
+                       @RequestParam(required = false, defaultValue = "0") int row,
+                       @RequestParam(required = false, defaultValue = "0") int remove) {
+
+        List<CartItem> cart = (List<CartItem>) session.getAttribute("cart");
+
+        if (remove > 0) {
+            cart.remove(row);
+        }
+
+        double totalAmount = 0;
+        if (cart != null) {
+            for (CartItem item : cart) {
+                totalAmount += item.product.price * item.quantity;
+            }
+        }
+
+        model.addAttribute("totalAmount", totalAmount);
 
         return "cart";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout(Model model, HttpSession session) {
+
+        List<CartItem> cart = (List<CartItem>) session.getAttribute("cart");
+
+        double totalAmount = 0;
+        if (cart != null) {
+            for (CartItem item : cart) {
+                totalAmount += item.product.price * item.quantity;
+            }
+        }
+
+        model.addAttribute("totalAmount", totalAmount);
+        model.addAttribute("numberOfItems", cart.size());
+
+        return "checkout";
     }
 }
