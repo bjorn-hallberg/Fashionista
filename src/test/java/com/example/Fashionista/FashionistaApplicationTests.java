b1d00369058bf91d401f3b7fab5bfef3fcf92d7f
Björn Hallberg <bjorn.hallberg@hm.com>
2020-11-06 15:17:47 +0100

    Improved unit test

--- src/test/java/com/example/Fashionista/FashionistaApplicationTests.java
+++ src/test/java/com/example/Fashionista/FashionistaApplicationTests.java
@@ -3,37 +3,38 @@ package com.example.Fashionista;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 import java.util.List;
 
 @SpringBootTest
 class FashionistaApplicationTests {
 
-	@Test
-	void testGetProducts() {
-		ProductRepository repository = new ProductRepository();
+    @Test
+    void testGetProducts() {
+        ProductRepository repository = new ProductRepository();
 
-		List<Product> products = repository.getProducts();
+        List<Product> products = repository.getProducts();
 
-		Assertions.assertNotNull(products);
-	}
+        Assertions.assertNotNull(products);
+    }
 
-	@Test
-	void testGetProductById() {
-		ProductRepository repository = new ProductRepository();
+    @Test
+    void testGetProductById() {
+        ProductRepository repository = new ProductRepository();
 
-		Product product = repository.getProduct(1L);
+        Product product = repository.getProduct(17L);
 
-		Assertions.assertEquals("Puff-sleeved sequined dress", product.name);
-	}
+        Assertions.assertEquals("Cable-knit cardigan", product.name);
+    }
 
-	@Test
-	void testGetProductsByCategory() {
-		ProductRepository repository = new ProductRepository();
+    @Test
+    void testGetProductsByCategory() {
+        ProductRepository repository = new ProductRepository();
 
-		List<Product> productsInCategory = repository.getProductsByCategory(Category.TSHIRTS);
+        List<Product> productsInCategory = repository.getProductsByCategory(Category.TSHIRTS);
 
-		Assertions.assertEquals(Category.TSHIRTS, productsInCategory.get(0).category);
-	}
+        for (Product product : productsInCategory) {
+            Assertions.assertEquals(Category.TSHIRTS, product.category);
+        }
+    }
 }
